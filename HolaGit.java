1eec7891c34c2a9291622ffa6e9a91a5926df871
AngCodee <angelo.cole75@gmail.com>
2026-04-02 21:23:21 +0200

    Agregar operación de incremento e imprimir declaración

--- HolaGit.java
+++ HolaGit.java
@@ -3,5 +3,10 @@ public class HolaGit {
      public static void main(String[] args) {
         System.out.println("Hola estoy aprendiendo GitHub");
         System.out.println("Creamos conflicto ");
+
+          int i=0;
+          i++;
+          System.out.println("Imprimimos el '1'--->" +i);
+          
      }
 }
