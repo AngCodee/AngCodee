666fd083c41f02b817dca60eba6765cc5f5af784
angCod75 <angelo.cole75@gmail.com>
2026-04-02 22:12:36 +0200

    Commit 2

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,13 @@ public class HolaMundo {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundoooooo!!!!!!!!!!!!");
+        int i=0;
+        int[] numeros = {1, 2, 3, 4, 5};
+
+        
+        for (int x : numeros) {
+            System.out.println("Numero "+i+":"+x);
+            i++;
+        }
     }
 } 
